42c22a7b005ee068ac20c0e6a7a2a052ccd39870
Jeff Dyck <jeff.c.dyck@gmail.com>
2017-09-02 15:06:05 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+﻿# PoE Trade Standards
+Simple chrome extension that adds two buttons, "Lower my standards" and "Up my standards" to poe.trade search results page.
+
+Clicking increments/decrements all numerical values specified in the search and re-searches for you.
+
+## TODO:
+* Allow changing of the diff value (Default 1)
+* Allow blacklisting certain fields
---
+++ poeTradeStandards.js
@@ -0,0 +1,40 @@
+﻿let DIFF = 1;
+
+BLACKLIST = [
+];
+
+let LOWER = "lower";
+let HIGHER = "higher";
+let SUBMIT_BUTTON = $("input:submit");
+let SEARCH_RESULTS = $("#content > div > div > div.live-search-box.alert-box");
+
+window.changeStandards = function(method) {
+    $.each($('input.num'),
+        function () {
+            elem = $(this);
+            let val = parseInt(elem.val());
+
+            if (!val) {
+                return;
+            }
+
+            if (method == LOWER) {
+                val -= DIFF;
+            }
+            else {
+                val += DIFF;
+            }
+
+            elem.val(val);
+
+        }
+    );
+
+    SUBMIT_BUTTON.click();
+}
+
+SEARCH_RESULTS.before('<a id="standards-lower-btn" class="button secondary">Lower my standards</a>');
+SEARCH_RESULTS.before('<a id="standards-up-btn" class="button secondary">Up my standards</a>');
+
+$("#standards-lower-btn").click(function () { changeStandards(LOWER); });
+$("#standards-up-btn").click(function () { changeStandards(HIGHER); });
